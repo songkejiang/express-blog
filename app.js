b5064e3ad732dbe2bfa00f249dfcd0c381da1e41
“songkejiang” <916348332@qq.com>
2019-10-14 22:52:50 +0800

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+let session = require('express-session')
+let redisStore = require('connect-redis')(session)
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 var userRouter = require('./routes/user');
@@ -19,6 +20,19 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+const redisClient = require('./db/redis')
+const sessionStore = new redisStore({
+  client: redisClient 
+})
+app.use(session({
+  secret: 'Jadfsf',
+  cookie: {
+    // path: '/', // 默认设置
+    // httpOnly: true, // 默认设置
+    maxAge: 24*3600*1000
+  },
+  store: sessionStore
+}))
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
--- conf/db.js
+++ conf/db.js
@@ -2,14 +2,18 @@
 const env = process.env.NODE_ENV
 
 let mysql_conf
-
+let redis_conf
 if(env==='dev') {
   mysql_conf = {
-    host: '149.129.107.253',
+    host: 'localhost',
     user: 'root',
-    password: 'Jks123456', 
+    password: 'Jks123456',
     port: '3306',
     database: 'test'
+  }
+  redis_conf = {
+    port: 6379,
+    host: '127.0.01'
   }
 }
 
@@ -21,8 +25,13 @@ if(env==='production') {
     port: '3306',
     database: 'myblog'
   }
+  redis_conf = {
+    port: 6379,
+    host: '127.0.01'
+  }
 }
 
 module.exports = {
-  mysql_conf
+  mysql_conf,
+  redis_conf
 }
